97c9f955ac972094ae5c533f8046ddc1a1de5328
aakash <aakash>
2021-02-17 20:22:38 +0530

    Step configured

--- src/main/java/com/opensource/springbatch/BatchConfig.java
+++ src/main/java/com/opensource/springbatch/BatchConfig.java
@@ -2,11 +2,24 @@ package com.opensource.springbatch;
 
 import javax.batch.api.listener.JobListener;
 
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BatchConfig {
+
+	@Autowired
+	private StepBuilderFactory sbf;
+
+	@Bean
+	public Step step1() {
+		return sbf.get("step1").<String, String>chunk(1).reader(reader()).processor(processor()).writer(writer())
+				.build();
+
+	}
 
 	// EXPOSE Reader writer and processor as beans
 	@Bean
