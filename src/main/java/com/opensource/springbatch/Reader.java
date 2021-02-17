f5ab1755b6f4b4e94a4a247f18c5751aef69567b
aakash <aakash>
2021-02-17 20:06:00 +0530

    Created item reader

---
+++ src/main/java/com/opensource/springbatch/Reader.java
@@ -0,0 +1,25 @@
+package com.opensource.springbatch;
+
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
+
+public class Reader implements ItemReader<String> {
+
+	private String[] courses = { "Java Web Services", "End to end project", "Angular" };
+	private int count;
+
+	@Override
+	public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+
+		if (count<courses.length) {
+			return courses[count++];
+		}else {
+			count=0;
+		}
+		return null;
+
+	}
+
+}
